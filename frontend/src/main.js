a60dda6a168c814fb8d1fb04d6f4c9eed267abe3
Léna Abel <lena31.abel@gmail.com>
2024-10-19 18:13:19 +0200

    Added git ignore

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,7 +1,21 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { BootstrapVue, IconsPlugin, BIcon } from 'bootstrap-vue';
+import axios from 'axios';
+
+// Import Bootstrap and BootstrapVue CSS files (order is important)
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
 
 Vue.config.productionTip = false
+
+// Make BootstrapVue available throughout the project
+Vue.use(BootstrapVue);
+Vue.use(IconsPlugin);
+Vue.component('BIcon', BIcon);
+
+axios.defaults.baseURL = 'http://localhost:3000';
+axios.defaults.headers.common['Access-Control-Allow-Origin'] = '*';
 
 new Vue({
   render: h => h(App),
