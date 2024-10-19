5f3a3175e4b4e305acba6889d75a313abca11b07
Léna Abel <lena31.abel@gmail.com>
2024-10-19 17:20:51 +0200

    Added server connection

--- backend/server.js
+++ backend/server.js
@@ -5,10 +5,16 @@ const bodyParser = require("body-parser");
 const mysql = require("mysql2");
 const server = express();
 
+const Routeur = require("./routes/routes.js");
+
 dotenv.config();
 
 server.use(cors());
 server.use(express.json());
 server.use(bodyParser.json());
 
+server.use("/", Routeur);
 
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
