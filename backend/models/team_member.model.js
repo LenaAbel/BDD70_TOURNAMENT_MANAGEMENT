fa67f20ffcf6b94d4ccedd35128cf3a1a35ff02c
bloodysh <anas.benmsatef@lycee-faure.fr>
2024-11-07 12:53:12 +0100

    db correction

--- backend/models/team_member.model.js
+++ backend/models/team_member.model.js
@@ -15,7 +15,7 @@ const addTeamMember = (team_id, player_id) => {
 // Obtenir les membres d'une équipe par ID d'équipe
 const getTeamMembersByTeamId = (team_id) => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT player.* FROM team_member JOIN player ON team_member.player_id = player.player_id WHERE team_member.team_id = ?', [team_id], (err, results) => {
+        db.query('SELECT * FROM team_member WHERE team_member.team_id = ?', [team_id], (err, results) => {
             if (err) {
                 return reject(err);
             }
@@ -42,7 +42,7 @@ const removeTeamMember = (team_id, player_id) => {
 // Obtenir toutes les équipes d'un joueur par ID de joueur
 const getTeamsByPlayerId = (player_id) => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT team.* FROM team_member JOIN team ON team_member.team_id = team.team_id WHERE team_member.player_id = ?', [player_id], (err, results) => {
+        db.query('SELECT * FROM team_member WHERE team_member.player_id = ?', [player_id], (err, results) => {
             if (err) {
                 return reject(err);
             }
@@ -54,7 +54,7 @@ const getTeamsByPlayerId = (player_id) => {
 // Obtenir tous les membres d'équipe avec détails
 const getAllTeamMembers = () => {
     return new Promise((resolve, reject) => {
-        db.query('SELECT team_member.*, player.name AS player_name, team.name AS team_name FROM team_member JOIN player ON team_member.player_id = player.player_id JOIN team ON team_member.team_id = team.team_id', (err, results) => {
+        db.query('SELECT * FROM team_member', (err, results) => {
             if (err) {
                 return reject(err);
             }
